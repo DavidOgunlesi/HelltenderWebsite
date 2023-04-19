97c564de7e80ac69e9e3df752b20ca962ca440ac
David Ogunlesi <david.ogunlesi@yahoo.co.uk>
2023-04-19 02:37:41 +0100

    initial commit

---
+++ carousel/createElements.js
@@ -0,0 +1,36 @@
+// Test to see if the browser supports the HTML template element by checking
+// for the presence of the template element's content attribute.
+if ('content' in document.createElement('template')) {
+    // Instantiate the table with the existing HTML tbody
+    // and the row with the template
+    const targetEl = document.querySelector(".carousel__list");
+    const template = document.querySelector('#carousel__template');
+
+    const gallery = [
+        { caption: "", image: "./gallery/wet_table.gif"},
+        { caption: "", image: "./gallery/bostonshaker.gif"},
+        { caption: "", image: "./gallery/characters.png"},
+        { caption: "", image: "./gallery/characters2.png"},
+        { caption: "", image: "./gallery/cups4.gif"},
+        { caption: "", image: "./gallery/cups6.gif"},
+        { caption: "", image: "./gallery/options.png"},
+        { caption: "", image: "./gallery/recipe_book.gif"},
+        { caption: "", image: "./gallery/wet_table.gif"},
+        { caption: "", image: "./gallery/wine.gif"},
+    ];
+
+    for (let i = 0; i < gallery.length; i++) {
+        // Clone the new row and insert it into the table
+        const clone = template.content.cloneNode(true);
+        let img = clone.querySelector("img");
+        img.src = gallery[i].image;
+        let caption = clone.querySelector(".slide__title");
+        caption.textContent = gallery[i].caption;
+
+        targetEl.appendChild(clone);
+    }
+
+} else {
+  // Find another way to add the rows to the table because
+  // the HTML template element is not supported.
+}
